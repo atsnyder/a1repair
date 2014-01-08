be5a43f3e872ab9b3f0d616739bc10cf121162a3
atsnyder <adam-snyder@uiowa.edu>
2014-01-08 17:56:23 -0600

    entering items

--- a1repairsite/A1.js
+++ a1repairsite/A1.js
@@ -1,8 +1,49 @@
 imgarray1 = new Array();
-imgarray1[0] = "images/shop.jpg";
-imgarray1[1] = "images/img2.jpg";
-imgarray1[2] = "images/img3.jpg";
+imgarray1[0] = "tn/welder/welder1.jpg";
+imgarray1[1] = "tn/welder/welder2.jpg";
+imgarray1[2] = "tn/welder/welder3.jpg";
+imgarray1[3] = "tn/welder/welder4.jpg";
 curr1 = 0;
+
+imgarray2 = new Array();
+imgarray2[0] = "tn/gtrac/gtrac1.jpg";
+imgarray2[1] = "tn/gtrac/gtrac2.jpg";
+imgarray2[2] = "tn/gtrac/gtrac3.jpg";
+imgarray2[3] = "tn/gtrac/gtrac4.jpg";
+imgarray2[4] = "tn/gtrac/gtrac5.jpg";
+curr2 = 0;
+
+imgarray3 = new Array();
+imgarray3[0] = "tn/coronet/coronet1.jpg";
+imgarray3[1] = "tn/coronet/coronet2.jpg";
+imgarray3[2] = "tn/coronet/coronet3.jpg";
+imgarray3[3] = "tn/coronet/coronet4.jpg";
+imgarray3[4] = "tn/coronet/coronet5.jpg";
+imgarray3[5] = "tn/coronet/coronet6.jpg";
+imgarray3[6] = "tn/coronet/coronet7.jpg";
+imgarray3[7] = "tn/coronet/coronet8.jpg";
+imgarray3[8] = "tn/coronet/coronet9.jpg";
+imgarray3[9] = "tn/coronet/coronet10.jpg";
+imgarray3[10] = "tn/coronet/coronet11.jpg";
+imgarray3[11] = "tn/coronet/coronet12.jpg";
+imgarray3[12] = "tn/coronet/coronet13.jpg";
+imgarray3[13] = "tn/coronet/coronet14.jpg";
+imgarray3[14] = "tn/coronet/coronet15.jpg";
+imgarray3[15] = "tn/coronet/coronet16.jpg";
+curr3 = 0;
+
+imgarray4 = new Array();
+imgarray4[0] = "tn/fifth/fifth1.jpg";
+imgarray4[1] = "tn/fifth/fifth2.jpg";
+imgarray4[2] = "tn/fifth/fifth3.jpg";
+imgarray4[3] = "tn/fifth/fifth4.jpg";
+curr4 = 0;
+
+imgarray5 = new Array();
+imgarray5[0] = "tn/minitr/minitr1.jpg";
+imgarray5[1] = "tn/minitr/minitr2.jpg";
+imgarray5[2] = "tn/minitr/minitr3.jpg";
+curr5 = 0;
 
 function nextImage(imgarr, curr, currid) {
 	var img = document.getElementById(currid);
@@ -72,4 +113,20 @@ function pI4() {
 		curr4 = imgarray4.length - 1;
 	}
 	nextImage(imgarray4, curr4, "saleimg4");
+}
+
+function nI5() {
+	curr5 = curr5 + 1;
+	if (curr5 == imgarray5.length){
+		curr5 = 0;
+	}
+	nextImage(imgarray5, curr5, "saleimg5");
+}
+
+function pI5() {
+	curr5 = curr5 - 1;
+	if (curr5 < 0){
+		curr5 = imgarray5.length - 1;
+	}
+	nextImage(imgarray5, curr5, "saleimg5");
 }
